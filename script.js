3d33e4cdac3db9c078d9e3f366b156a42dffe1f3
Isa <Isaismail3000@gmail.com>
2023-05-19 23:28:23 +0300

    Create module to generate game board using 2D array

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let btn = document.querySelector("button");
-console.log(btn);
+let gameBoard = (() => {
+  const board = [];
+
+  const rows = 3;
+  const columns = 3;
+
+  for (i = 0; i < rows; i++) {
+    board[i] = [];
+    for (j = 0; j < columns; j++) {
+      board[i].push("0");
+    }
+  }
+  console.log(board);
+})();
