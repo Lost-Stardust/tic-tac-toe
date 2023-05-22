99171cd727b603dac2a85711200316b545c5ad46
Isa <Isaismail3000@gmail.com>
2023-05-22 22:12:44 +0300

    Clear out the code inside the modules for a new approach

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-const cell = (() => {
-  let value = 0;
+const cell = (() => {})();
 
-  const addValue = (x) => (value = x);
-  const getValue = () => value;
+const gameBoard = (() => {})();
 
-  return { getValue, addValue };
-})();
-console.log(cell.getValue());
-
-const gameBoard = (() => {
-  const board = [];
-
-  const rows = 3;
-  const columns = 3;
-
-  for (i = 0; i < rows; i++) {
-    board[i] = [];
-    for (j = 0; j < columns; j++) {
-      board[i].push(cell.getValue());
-    }
-  }
-  console.table(board);
-})();
-
-const generatePlayer = (name, symbol) => {
-  return { name, symbol };
-};
-const player1 = generatePlayer("Miyamoto Musashi", "$");
-console.log(player1.name, player1.symbol);
+const generatePlayer = () => {};
