04a9565d80eb1636efa2258b0843c4f02a63689c
Isa <Isaismail3000@gmail.com>
2023-05-24 19:37:22 +0300

    Create module for changing pages, add eventListener on button in home

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const page = (() => {
+  const home = document.querySelector(".home");
+
+  function change() {
+    home.style.setProperty("display", "none");
+  }
+
+  return { change };
+})();
+
 const cell = (() => {})();
 
 const gameBoard = (() => {
-  function printBoard() {
+  function print() {
     for (let i = 0; i < 3; i++) {
       let board = document.querySelector(".board");
       for (let j = 0; j < 3; j++) {
@@ -11,7 +21,13 @@ const gameBoard = (() => {
     }
   }
 
-  return { printBoard };
+  return { print };
 })();
+
+const btn = document.querySelector("#change");
+btn.addEventListener("click", () => {
+  page.change();
+  gameBoard.print();
+});
 
 const generatePlayer = () => {};
