a2121f1a0ef12355711e4db3ab7aa579d249f27b
Isa <Isaismail3000@gmail.com>
2023-05-24 17:43:25 +0300

    Create a function inside gameBoard that will print the board

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const cell = (() => {})();
 
-const gameBoard = (() => {})();
+const gameBoard = (() => {
+  function printBoard() {
+    for (let i = 0; i < 3; i++) {
+      let board = document.querySelector(".board");
+      for (let j = 0; j < 3; j++) {
+        let btn = document.createElement("button");
+        board.appendChild(btn);
+      }
+    }
+  }
+
+  return { printBoard };
+})();
 
 const generatePlayer = () => {};
