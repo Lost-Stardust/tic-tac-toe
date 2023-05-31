7638020cc330bfc5da92c291d68369987cedb882
Isa <Isaismail3000@gmail.com>
2023-05-31 23:11:17 +0300

    Comment unnecessary code, create playerFactory, rework print function

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-const page = (() => {
-  const home = document.querySelector(".home");
+// const page = (() => {
+//   const home = document.querySelector(".home");
 
-  function change() {
-    home.style.setProperty("display", "none");
-  }
+//   function change() {
+//     home.style.setProperty("display", "none");
+//   }
 
-  return { change };
-})();
+//   return { change };
+// })();
 
-const cell = (() => {})();
+const playerFactory = (name, symbol) => {
+  return { name, symbol };
+};
 
 const gameBoard = (() => {
   function print() {
     for (let i = 0; i < 3; i++) {
-      let board = document.querySelector(".board");
+      const row = document.createElement("div");
+      row.className = "row";
+      const board = document.querySelector(".board");
+      board.appendChild(row);
+
       for (let j = 0; j < 3; j++) {
-        let btn = document.createElement("button");
-        board.appendChild(btn);
+        const column = document.createElement("button");
+        column.className = "column";
+        row.appendChild(column);
       }
     }
   }
 
   return { print };
 })();
+gameBoard.print();
 
-const btn = document.querySelector("#change");
-btn.addEventListener("click", () => {
-  page.change();
-  gameBoard.print();
-});
-
-const generatePlayer = () => {};
+// const btn = document.querySelector("#change");
+// btn.addEventListener("click", () => {
+//   page.change();
+//   gameBoard.print();
+// });
