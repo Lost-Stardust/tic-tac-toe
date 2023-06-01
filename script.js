fc06293d01e51a21fa2ca24517bc858ee2da96aa
Isa <Isaismail3000@gmail.com>
2023-06-01 20:32:52 +0300

    Create gameController module, add another parameter to playerFactory
    gameController is responsible for creating the players and has a
    private function for alternating turns between player1 and player2.
    It also appends an eventListener to all the cells in the board.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@
 //   return { change };
 // })();
 
-const playerFactory = (name, symbol) => {
-  return { name, symbol };
+const playerFactory = (name, symbol, status) => {
+  return { name, symbol, status };
 };
 
 const gameBoard = (() => {
@@ -31,6 +31,42 @@ const gameBoard = (() => {
   return { print };
 })();
 gameBoard.print();
+
+const gameController = (() => {
+  // Make player function; for later
+  // it will take input from user for names
+  function makePlayer() {}
+
+  // Temporary player creations
+  const P1 = playerFactory("Joestar", "X", true);
+  const P2 = playerFactory("Bitch", "O");
+
+  function _turn() {
+    if (P1.status == true) {
+      P1.status = false;
+      P2.status = true;
+    } else if (P2.status == true) {
+      P2.status = false;
+      P1.status = true;
+    }
+  }
+
+  const cells = document.querySelectorAll(".column");
+  console.log(cells);
+  cells.forEach((cell) => {
+    console.log(cell);
+    cell.addEventListener("click", (e) => {
+      if (P1.status == true) {
+        console.log(e.target);
+        e.target.textContent = P1.symbol;
+        _turn();
+      } else if (P2.status == true) {
+        e.target.textContent = P2.symbol;
+        _turn();
+      }
+    });
+  });
+})();
 
 // const btn = document.querySelector("#change");
 // btn.addEventListener("click", () => {
