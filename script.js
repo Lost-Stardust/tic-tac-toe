d5350f0fd4eb3d2db40327f24f4f0564e046e338
Isa <Isaismail3000@gmail.com>
2023-06-17 20:08:18 +0300

    Add winner name to span in modal and set modal/overlay active on win

--- script.js
+++ script.js
@@ -176,93 +176,164 @@ const validate = (() => {
       currPlayer = players.P2.name;
     }
 
+    const span = document.querySelector("#winner");
+    console.log(span);
+
     // variable that will store winners name
     let winner;
 
+    const gameOver = document.querySelector("#gameover");
+    const overlay = document.querySelector("#overlay");
+
     // patterns for 'X'
     if (board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
+      console.log(span);
       cells[0].style.color = "#41ead4";
       cells[3].style.color = "#41ead4";
       cells[6].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[1].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[7].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[2].style.color = "#41ead4";
       cells[5].style.color = "#41ead4";
       cells[8].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[0].style.color = "#41ead4";
       cells[1].style.color = "#41ead4";
       cells[2].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[3].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[5].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[6].style.color = "#41ead4";
       cells[7].style.color = "#41ead4";
       cells[8].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[0].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[8].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[2].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[6].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     }
 
     // patterns for 'O'
     if (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[0].style.color = "#41ead4";
       cells[3].style.color = "#41ead4";
       cells[6].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[1].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[7].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[2].style.color = "#41ead4";
       cells[5].style.color = "#41ead4";
       cells[8].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[0].style.color = "#41ead4";
       cells[1].style.color = "#41ead4";
       cells[2].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[3].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[5].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[6].style.color = "#41ead4";
       cells[7].style.color = "#41ead4";
       cells[8].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[0].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[8].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     } else if (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O") {
-      console.log((winner = currPlayer));
+      winner = currPlayer;
+      span.textContent = winner;
       cells[2].style.color = "#41ead4";
       cells[4].style.color = "#41ead4";
       cells[6].style.color = "#41ead4";
+
+      gameOver.classList.add("active");
+      overlay.classList.add("active");
     }
 
     // variable that will determine draw if it's value is 9
